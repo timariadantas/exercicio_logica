5e1e1cebe784e6611ae9cea3b1c285532e6786f7
Maria Dantas <mariadantasti@gmail.com>
2026-05-03 23:03:09 -0300

    feat: Att Program.cs

--- exercicio_logico/Program.cs
+++ exercicio_logico/Program.cs
@@ -3,10 +3,29 @@ using exercicios_logico;
 
 
 
-class Program
+ Palindromo.Verificar("ana");
+Palindromo.Verificar("pedro");
+CaixaEletronico.Sacar(120);
+Atm.Sacar(120);
+Atm.Sacar(30);
+ContarCarater.Contar("banana");
+
+var service = new AssentoService();
+
+service.CriarAssentos(5);
+Console.WriteLine("\nTentando reservar assento 1...");
+var sucesso = service.ReservarAssento(1);
+
+if (sucesso)
+    Console.WriteLine("Reserva realizada com sucesso!");
+else
+    Console.WriteLine("Erro ao reservar assento.");
+
+Console.WriteLine("\n Lista de assentos ");
+    var lista = service.ListarAssentos();
+    foreach (var a in lista)
 {
-    static void Main()
-    {
-        
-    }
+    Console.WriteLine($"Assento {a.Id} - {a.Status}");
 }
+
+
